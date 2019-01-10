79bd662c919347eb8e9a5be2d03eeebcb9e82e0e
Robert Davoyan <aleqsanyanarthur@gmail.com>
2019-01-10 16:48:01 +0400

    Add the login and registration layouts,trusting to connect back to front

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,49 +13,48 @@ const Users = require('../../models/Users');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
-// @route  POST api/users/register
-// @desc   Registration users route
-// @access Public
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
 router.post('/register', (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
-
-    // Check Validation 
+  
+    // Check Validation
     if (!isValid) {
       return res.status(400).json(errors);
     }
-
-
-    const avatar = gravatar.url(req.body.email , {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
+  
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // Size
+          r: 'pg', // Rating
+          d: 'mm' // Default
+        });
+  
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+  
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
     });
-    Users.findOne({email : req.body.email})
-        .then(user =>{
-            // const errors = {}
-            if(user) {
-                errors.email = 'Email is already exist';
-                return res.status(400).json(errors.email);
-            } else{
-                const newUser = new Users({
-                    name : req.body.name,
-                    email : req.body.email,
-                    password : req.body.password,
-                    avatar,
-                });
-                
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) throw err;
-                        newUser.password = hash;
-                        newUser.save()
-                            .then(user => res.json(user))
-                            .catch(err => console.log(err));
-                    })
-                });
-            }
-        } )
-});
+  });
 
 // @route  POST api/users/login
 // @desc   Login users route / JWT token
--- server.js
+++ server.js
@@ -1,39 +1,49 @@
 const express = require('express');
-const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
-// Initing App
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
-// Required app routes
-const users = require('./routes/api/users');
-const posts = require('./routes/api/posts');
-const profile = require('./routes/api/profile');
-
-// Db config
-const db = require('./config/keys').dbURI;
-
-// db connect to mongoose 
-mongoose
-    .connect(db)
-    .then(() => console.log('DB is connected'))
-    .catch((err) => console.log(err));
-
-// Passport Midleware
-app.use(passport.initialize());
-
-// Passport config
-require('./config/passport')(passport);
-
-// App use body-parser midleware
-app.use(bodyParser.urlencoded({extended : false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// App use routes
-app.use('/api/users',users);
-app.use('/api/posts',posts);
-app.use('/api/profile',profile);
+// DB Config
+const db = require('./config/keys').dbURI;
 
-app.listen(port, (req, res) => console.log(`Server runing on ${port} port`));
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
